12b5f947472dd4b1524789f7a54bf11214e41219
Nicholas Zivkovic <znichola@student.42nice.fr>
2025-06-24 18:25:50 +0200

    initial jupyter setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dlsr
+
+This is a group project but I will start it solo to get things rolling. I would also like to use jupyter notebook for this project to have a nice presentation of the code and functionality.
+
+
---
+++ make.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+fetch() {
+    wget https://cdn.intra.42.fr/document/document/34001/datasets.tgz
+    tar -xf datasets.tgz
+    rm datasets.tgz
+}
+
+venv() {
+    python3 -m venv .venv \
+        && source .venv/bin/activate \
+        && pip install --upgrade pip \
+        && pip install -r requirements.txt
+
+    echo "ℹ️ To activate the venv: run : source .venv/bin/activate"
+}
+
+usage() {
+    cmds=$(declare -F | awk '{print $3}' | paste -s -d'|' -)
+    echo "Usage: ./make.sh [$cmds]"
+}
+
+
+# ENTRY POINT
+
+cmd="$1"
+
+if [[ -z "$cmd" || "$cmd" == "help" ]]; then
+    usage
+    exit 0
+fi
+
+# Check if function exists, then call
+if declare -F "$cmd" > /dev/null; then
+    "$cmd"
+else
+    echo "Unknown command: $cmd"
+    echo
+    usage
+    exit 1
+fi
