63bcee4be786fbb21c7d9c7b995bdbd29b825519
Maxime Gayout <mgayout@c2r11p1.42nice.fr>
2026-01-06 11:40:18 +0100

    training loop

--- test.py
+++ test.py
@@ -2,35 +2,43 @@ from describe import loadData, describe
 import math
 import sys
 
-class House_Prediction:
+# class House_Prediction:
 
-    def __init__(self, data, house):
-        self.weights = [0 for _ in range(len(data.keys()))]
-        self.tmp_weights = self.weights.copy()
-        self.bias = 0
-        self.tmp_bias = self.bias
-        self.house_to_predict = house
-    
-    def train(self, data, house_data, batch):
-        errors = []
-        for i in range(batch):
-            y, x = self.h(data[i])
-            z = 1 if self.house_to_predict == house_data[1] else 0
-            errors.append(self.calcul(x, z))
-        self.tmp_bias -= (- 1 / len(errors)) * sum(errors) # ? maybe
-        self.tmp_weights = [w * e for w, e in zip(self.weights, errors)]
+#     def __init__(self, data, house):
+#         self.weights = [0 for _ in range(len(data.keys()))]
+#         self.tmp_weights = self.weights.copy()
+#         self.bias = 0
+#         self.tmp_bias = self.bias
+#         self.house_to_predict = house
 
-    def calcul(self, x, z):
-        return max(x, 0) - x * z + math.log(1 + math.exp(-abs(x)))
+#     def gradient_descent(self, data, house_data, learning_rate):
+#         theta = self.weights
+#         X = data
+#         Y = 1 if self.house_to_predict == house_data[1] else 0
+#         alpha = learning_rate
+#         return
 
-            
-    def h(self, data_i):
-        x = self.bias + sum(d * w for d, w in zip(data_i, self.weights))
-        return self.g(x), x
+    # def train(self, data, house_data, batch):
+    #     errors = []
+    #     for i in range(batch):
+    #         y, x = self.h(data[i])
+    #         z = 1 if self.house_to_predict == house_data[1] else 0
+    #         errors.append(self.calcul(x, z))
+    #     self.tmp_bias -= (- 1 / len(errors)) * sum(errors) # ? maybe
+    #     self.tmp_weights = [w * e for w, e in zip(self.weights, errors)]
 
-    def g(self, z):
-        '''Sigmoid = 1 / (1 + e-z)'''
-        return 1 / (1+ math.exp(-z))
+    # def calcul(self, x, z):
+    #     return max(x, 0) - x * z + math.log(1 + math.exp(-abs(x)))
+
+
+    # def h(self, data_i):
+    #     #with ou without bias
+    #     x = self.bias + sum(d * w for d, w in zip(data_i, self.weights))
+    #     return self.g(x), x
+
+    # def g(self, z):
+    #     '''Sigmoid = 1 / (1 + e-z)'''
+    #     return 1 / (1+ math.exp(-z))
 
 class logistic_regression:
 
@@ -43,10 +51,14 @@ class logistic_regression:
         self.houses_data = self.data.pop("Hogwarts House")
         self.houses = list(set(self.houses_data))
         self.subjects = list(self.data.keys())
-        self.House_Predictors = [House_Prediction(self.data, house) for house in self.houses]
+        self.dataToArray()
+        # self.House_Predictors = [House_Prediction(self.data, house) for house in self.houses]
         self.learning_rate = 0.1
         self.max_epoch = 10000
         self.batch = 1
+
+        self.weights = [[0 for _ in range(len(self.subjects))] for _ in self.houses]
+
         self.train()
 
     def isHouse(self, house, index):
@@ -60,6 +72,9 @@ class logistic_regression:
         for subject_key in subject_dict.keys():
             subject_dict[subject_key] = [grade for grade in subject_dict[subject_key].values()]
         return subject_dict
+
+    def dataToArray(self):
+        self.data = [d for d in self.data.values()]
 
     def cleanUpData(self, df):
         return df.drop(columns=self.columns_to_drop, errors="ignore").dropna()
@@ -89,23 +104,55 @@ class logistic_regression:
 
 
     def train(self):
-        
-        epoch = 0
-        while epoch < self.max_epoch:
-            for i in range(self.batch, len(self.houses_data)):
-                for house_predictor in self.House_Predictors:
-                    house_predictor.train(self.data[i-self.batch:i], self.houses_data[i-self.batch:i], self.batch)
-            epoch += 1
+        for _ in range(self.max_epoch):
+            for i, house in enumerate(self.houses):
+                self.weights[i] = self.gradient_descent(self.weights[i], house)
+            # print(self.weights)
+            print("------------------------------------------------")
         self.save()
 
+    def gradient_descent(self, weights, house_to_predict):
+        theta = weights
+        x__ = self.data
+        y_ = [1 if house_to_predict == h else 0 for h in self.houses_data]
+        alpha = self.learning_rate
+        print(house_to_predict, " ", end="")
+        gradient = self.gradient(theta, x__, y_)
+
+        return [t - alpha * g for t, g in zip(theta, gradient)]
+    
+    def gradient(self, theta, x__, y_):
+        m = len(y_)
+        grad = [0.0] * len(theta)
+        total_loss = 0
+
+        for x_, y in zip(x__, y_):
+            h = self.hypothesis(theta, x_)
+            error = h - y
+            for j in range(len(theta)):
+                grad[j] += error * x_[j]
+            total_loss += error
+        print(total_loss)
+        return [g / m for g in grad]
+
+    def hypothesis(self, theta, x_):
+        z = sum(t * x for t, x in zip(theta, x_))
+        return self.sigmoid(z)
+
+    def sigmoid(self, z):
+        '''Sigmoid = 1 / (1 + e-z)'''
+        return 1 / (1+ math.exp(-z))
 
     def save(self):
         return
+        
+
 
 
 if __name__ == "__main__":
-    try:
-        logistic_regression("datasets/dataset_train.csv")
-    except Exception as err:
-        print(err)
+    logistic_regression("datasets/dataset_train.csv")
+    # try:
+    #     logistic_regression("datasets/dataset_train.csv")
+    # except Exception as err:
+    #     print(err)
 
